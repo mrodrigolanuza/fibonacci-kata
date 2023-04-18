04fc8e07c417c6f4dac754da8a3470430995045f
Marcos Rodrigo <marcosrodrigolanuza@gmail.com>
2023-04-18 17:07:32 +0200

    Green: when any other number than zero or one, returns the sum of the two preceding numbers

--- src/core/fibonacci.ts
+++ src/core/fibonacci.ts
@@ -5,5 +5,5 @@ export function getFibonacciNumber(number:number){
     if(number === 1)
         return 1;
     
-    return (number-1)+(number-2);
+    return getFibonacciNumber(number-1) + getFibonacciNumber(number-2);
 }
