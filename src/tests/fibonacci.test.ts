8d3938a671402af9776c3d4fb8a9fcf5447f9cc8
Marcos Rodrigo <marcosrodrigolanuza@gmail.com>
2023-04-18 16:26:31 +0200

    Red: when receives one returns one

--- src/tests/fibonacci.test.ts
+++ src/tests/fibonacci.test.ts
@@ -4,4 +4,7 @@ describe("The Fibonacci sequence", () => {
     it("when receives zero returns zero", () => {
         expect(getFibonacciNumber(0)).toBe(0);
     });
+    it("when receives one returns one", () => {
+        expect(getFibonacciNumber(1)).toBe(1);
+    });
 });
